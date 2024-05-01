cfa328b350367cd29325817838a48c2cdac53e83
Kaka Ruto <kaka@collinsruto.com>
2024-05-01 14:59:03 +0100

    Initial astronvim v4 setup

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,49 @@
+-- Customize Treesitter
+
+---@type LazySpec
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    -- add more things to the ensure_installed table protecting against community packs modifying it
+    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+      "lua",
+      "vim",
+      -- add more arguments for adding more treesitter parsers
+    })
+  end,
+    -- Location and syntax-aware text objects
+  {
+    "RRethy/nvim-treesitter-textsubjects",
+    after = "nvim-treesitter",
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        textsubjects = {
+          enable = true,
+          prev_selection = ",", -- (Optional) keymap to select the previous selection
+          keymaps = {
+            ["."] = "textsubjects-smart",
+            [";"] = "textsubjects-container-outer",
+            ["i;"] = "textsubjects-container-inner",
+          },
+        },
+      }
+    end,
+  },
+  -- Show code context
+  {
+    "romgrk/nvim-treesitter-context",
+    after = "nvim-treesitter",
+  },
+  {
+    -- Better commenting
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    after = "nvim-treesitter",
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        context_commentstring = {
+          enable = true,
+        },
+      }
+    end,
+  },
+}
